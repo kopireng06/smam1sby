de6dcdb874f545bd63a8a8a39ae4e2889c1cd4df
paijolongor <zidankarim5@gmail.com>
2021-02-24 10:14:19 +0700

    CAROUSEL TEROSSS

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -20,13 +20,44 @@ class CarouselController extends Controller
         $image = $request->file('foto_car');
         $imageName = time().'.'.$image->extension();
         $image->move(public_path('images'),$imageName);
+
         $judul = $request->judul_car;
         $isi = $request->isi_car;
         $carousel = new Carousel();
-        $carousel->foto_car = '/public/images/'.$imageName;
+        $carousel->foto_car = $imageName;
         $carousel->judul_car = $judul;
         $carousel->isi_car = $isi;
         $carousel->save();
         return redirect("/carousel");
+    }
+
+    public function edit($id_car)
+    {
+        $carousel = \App\Models\Carousel::find($id_car);
+        return view('carousel.edit',compact('carousel'));
+    }
+
+    public function update(Request $request)
+    {
+        $image = $request->file('foto_car');
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('images'),$imageName);
+
+        $judul = $request->judul_car;
+        $isi = $request->isi_car;
+        $carousel = new Carousel();
+        $carousel = \App\Models\Carousel::find($request->id_car);
+        $carousel->foto_car = $imageName;
+        $carousel->judul_car = $judul;
+        $carousel->isi_car = $isi;
+        $carousel->save();
+        return redirect('/carousel');
+    }
+
+    public function delete($id_car)
+    {
+        $carousel = \App\Models\Carousel::find($id_car);
+        $carousel->delete();
+        return redirect('/carousel');
     }
 }
---
+++ resources/views/carousel/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.dashboardtemplate')
+@section('title')
+    <title>contohadmin</title>
+@endsection
+@section('konten')
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel">
+					    <div class="panel-heading">
+                            <h1 class="panel-title">Edit Data Carousel</h1>
+                        </div>
+                            
+                        <div class="panel-body">
+                            
+                            <form action="/carousel/{{$carousel->id_car}}/update" method="POST" enctype="multipart/form-data">
+                                <div class="modal-body">
+                                
+                                    {{csrf_field()}}
+                    
+                                    <div class="form-group">
+                                        <label for="file">Choose File Image</label>
+                                        <input type="file" name="foto_car" class="form-control" onchange="previewFile(this)" value="{{$carousel->foto_car}}"/>
+                                        <img id="previewImg" alt="Image" src="{{asset('images')}}/{{$carousel->foto_car}}"style="max-width:130px;margin-top:20px;"/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="file">Judul</label>
+                                        <input type="text" name="judul_car" class="form-control" value="{{$carousel->judul_car}}" required/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="file">Isi</label>
+                                        <input type="text" name="isi_car" class="form-control" value="{{$carousel->isi_car}}" required/>
+                                    </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-warning">Save</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
