9a8671cb837e22da86446a8d6f0723d42130aa92
kopireng06 <naufalghaniachmani@gmail.com>
2021-03-11 12:48:29 +0700

    foto bar

--- resources/views/smam1sby.blade.php
+++ resources/views/smam1sby.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>SMA Muhammadiyah 1 Surabaya</title>
+    <link rel="shortcut icon" href="{{asset('image/logo-sma-2.png')}}" />
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     {{-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> --}}
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
